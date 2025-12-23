77c993179e2db39485480269f98e7d3afed6e6ca
Fabrice Triboix <ft+github@falcon1.ltd>
2025-12-23 21:13:30 +0000

    bugfix

--- modules/bootstrap/kms.tf
+++ modules/bootstrap/kms.tf
@@ -1,10 +1,10 @@
 module "key" {
   source = "git::https://github.com/fabricetriboix/terraform-aws-kms.git?ref=v4.1.1-1"
 
-  description              = "Key to encrypt OpenTofu states and DynamoDB lock tables"
-  aliases                  = ["tf"]
-  deletion_windows_in_days = 7
-  rotation_period_in_days  = 30
+  description             = "Key to encrypt OpenTofu states and DynamoDB lock tables"
+  aliases                 = ["tf"]
+  deletion_window_in_days = 7
+  rotation_period_in_days = 30
 
   tags = merge(local.tags, {
     Name = "alias/tf"
