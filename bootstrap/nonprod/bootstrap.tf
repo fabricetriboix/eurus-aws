ef0d6b8cde831618b8fe791664b3293c6e29acc1
Fabrice Triboix <ft+github@falcon1.ltd>
2025-12-28 11:42:56 +0000

    fix(bootstrap): Fix bootstrap module version

--- bootstrap/nonprod/bootstrap.tf
+++ bootstrap/nonprod/bootstrap.tf
@@ -1,6 +1,6 @@
 module "bootstrap" {
   # checkov:skip=CKV_TF_1,CKV_TF_2:False positives
-  source = "git::https://github.com/fabricetriboix/eurus-aws.git//modules/bootstrap?ref=feat-bootstrap"
+  source = "git::https://github.com/fabricetriboix/eurus-aws.git//modules/bootstrap?ref=modules-bootstrap-v0.1.0"
 
   org     = "myorg"
   project = "myproject"
