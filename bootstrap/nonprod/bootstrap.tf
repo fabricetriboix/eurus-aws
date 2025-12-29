cda99ca2c5e1e457ad06edcc50e7b759a0f06bbf
Fabrice Triboix <ft+github@falcon1.ltd>
2025-12-29 11:16:06 +0000

    feat(bootstrap): Depoy to nonprod

--- bootstrap/nonprod/bootstrap.tf
+++ bootstrap/nonprod/bootstrap.tf
@@ -1,6 +1,6 @@
 module "bootstrap" {
   # checkov:skip=CKV_TF_1,CKV_TF_2:False positives
-  source = "git::https://github.com/fabricetriboix/eurus-aws.git?ref=module-bootstrap-v0.1.1"
+  source = "git::https://github.com/fabricetriboix/eurus-aws.git?ref=module-bootstrap-v0.1.2"
 
   org     = "myorg"
   project = "myproject"
