7f36b75b792ce0128e1d259be84d837e3f4175ad
Fabrice Triboix <ft+github@falcon1.ltd>
2026-01-30 11:48:08 +0000

    feat(bootstrap): Remove DynamoDB table for tofu locks

--- module/bootstrap/kms.tf
+++ module/bootstrap/kms.tf
@@ -2,7 +2,7 @@ module "key" {
   # checkov:skip=CKV_TF_1,CKV_TF_2:False positives
   source = "git::https://github.com/fabricetriboix/terraform-aws-kms.git?ref=v4.1.1-1"
 
-  description             = "Key to encrypt OpenTofu states and DynamoDB lock tables"
+  description             = "Key to encrypt OpenTofu state files"
   aliases                 = [local.kms_alias]
   deletion_window_in_days = 7
   rotation_period_in_days = 90
