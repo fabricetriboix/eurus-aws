93da7b946cf150b968348d229923d492996556ec
Fabrice Triboix <ft+github@falcon1.ltd>
2026-03-03 09:26:21 +0000

    Another workaround

--- module/bootstrap/tf-iam.tf
+++ module/bootstrap/tf-iam.tf
@@ -20,19 +20,8 @@ resource "aws_iam_role" "tf_role" {
 }
 
 # NB: You will need to tighten the OpenTofu policy according to your compliance and security requirements.
-data "aws_iam_policy_document" "tf_role_policy" {
-  statement {
-    actions   = ["*"]
-    resources = ["*"]
-  }
-}
-
-resource "aws_iam_policy" "tf_role_policy" {
-  name   = "tf-role-policy-${var.region}"
-  policy = data.aws_iam_policy_document.tf_role_policy.json
-}
-
 resource "aws_iam_role_policy_attachment" "tf_role_attachment" {
+  # checkov:skip=CKV_AWS_274
   role       = aws_iam_role.tf_role.name
-  policy_arn = aws_iam_policy.tf_role_policy.arn
+  policy_arn = "arn:aws:iam::aws:policy/AdministratorAccess"
 }
