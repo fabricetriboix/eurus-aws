353666998e4aee2f7b1bc429262e624a18e6731a
Fabrice Triboix <16716041+fabricetriboix@users.noreply.github.com>
2026-06-23 17:39:39 +0100

    feat(amp): Add Amazon Managed Prometheus feature (#70)

---
+++ feature/amp/amp.tf
@@ -0,0 +1,30 @@
+# Amazon Managed Prometheus
+
+resource "aws_cloudwatch_log_group" "this" {
+  name       = "/amp/${var.org}-${var.project}-${var.env}"
+  region     = var.region
+  kms_key_id = module.key.key_arn
+
+  # checkov:skip=CKV_AWS_338:Retention of less than one year is allowed
+  retention_in_days = var.log_retention_days
+
+  tags = merge(local.tags, {
+    Name    = "/amp/${var.org}-${var.project}-${var.env}"
+    Purpose = "Amazon Managed Prometheus log group for ${var.org}-${var.project}-${var.env}"
+  })
+}
+
+resource "aws_prometheus_workspace" "this" {
+  alias       = "${var.org}-${var.project}-${var.env}"
+  region      = var.region
+  kms_key_arn = module.key.key_arn
+
+  logging_configuration {
+    log_group_arn = aws_cloudwatch_log_group.this.arn
+  }
+
+  tags = merge(local.tags, {
+    Name    = "${var.org}-${var.project}-${var.env}"
+    Purpose = "Amazon Managed Prometheus workspace for ${var.org}-${var.project}-${var.env}"
+  })
+}
---
+++ feature/amp/kms.tf
@@ -0,0 +1,15 @@
+module "key" {
+  # checkov:skip=CKV_TF_1,CKV_TF_2:False positives
+  source = "git::https://github.com/fabricetriboix/terraform-aws-kms.git?ref=v4.1.1-1"
+
+  description             = "Key to encrypt Amazon Managed Prometheus data"
+  region                  = var.region
+  aliases                 = ["amp"]
+  deletion_window_in_days = 7
+  rotation_period_in_days = 90
+
+  tags = merge(local.tags, {
+    Name    = "alias/amp",
+    Purpose = "Encrypt Amazon Managed Prometheus data"
+  })
+}
