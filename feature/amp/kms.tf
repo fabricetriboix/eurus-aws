2eab5fe79afd3733d9140edc9502c582c3e7c873
Fabrice Triboix <16716041+fabricetriboix@users.noreply.github.com>
2026-07-04 10:16:54 +0100

    fix(amp): Deploy AMP to common-nonprod (#71)

--- feature/amp/kms.tf
+++ feature/amp/kms.tf
@@ -8,6 +8,78 @@ module "key" {
   deletion_window_in_days = 7
   rotation_period_in_days = 90
 
+  key_statements = [
+    {
+      sid     = "Root"
+      actions = ["kms:*"]
+      principals = [
+        {
+          type        = "AWS"
+          identifiers = ["arn:aws:iam::${local.account_id}:root"]
+        }
+      ]
+      resources = ["*"]
+    },
+    {
+      sid = "CloudWatchLogs"
+      actions = [
+        "kms:Encrypt*",
+        "kms:Decrypt*",
+        "kms:ReEncrypt*",
+        "kms:GenerateDataKey*",
+        "kms:Describe*",
+      ]
+      resources = ["*"]
+
+      principals = [
+        {
+          type        = "Service"
+          identifiers = ["logs.${var.region}.amazonaws.com"]
+        }
+      ]
+
+      condition = [
+        {
+          test     = "ArnLike"
+          variable = "kms:EncryptionContext:aws:logs:arn"
+          values = [
+            "arn:aws:logs:${var.region}:${local.account_id}:log-group:/amp/*",
+          ]
+        }
+      ]
+    },
+    {
+      sid = "AmazonManagedPrometheus"
+      actions = [
+        "kms:DescribeKey",
+        "kms:CreateGrant",
+        "kms:GenerateDataKey",
+        "kms:Decrypt",
+      ]
+      resources = ["*"]
+
+      principals = [
+        {
+          type        = "AWS"
+          identifiers = ["*"]
+        }
+      ]
+
+      condition = [
+        {
+          test     = "StringEquals"
+          variable = "kms:ViaService"
+          values   = ["aps.${var.region}.amazonaws.com"]
+        },
+        {
+          test     = "StringEquals"
+          variable = "kms:CallerAccount"
+          values   = [local.account_id]
+        },
+      ]
+    },
+  ]
+
   tags = merge(local.tags, {
     Name    = "alias/amp",
     Purpose = "Encrypt Amazon Managed Prometheus data"
