9c1469ad6fdded2f847abfba056414b9205cb1c9
Fabrice Triboix <ft+github@falcon1.ltd>
2026-07-10 12:17:18 +0100

    feat(ecs): Fix TF code

--- feature/ecs-plf/ecs-plf.tf
+++ feature/ecs-plf/ecs-plf.tf
@@ -2,9 +2,10 @@ module "ecs_cluster" {
   #source = "git::https://github.com/fabricetriboix/eurus-aws.git//?ref=feature-ecs-cluster-v0.1.0"
   source = "git::https://github.com/fabricetriboix/eurus-aws.git//module/ecs-cluster?ref=feat-ecs-cluster"
 
-  org                = var.org
-  project            = var.project
-  env                = var.env
-  region             = var.region
-  log_retention_days = var.log_retention_days
+  org                 = var.org
+  project             = var.project
+  env                 = var.env
+  region              = var.region
+  cluster_name_suffix = "plf"
+  log_retention_days  = var.log_retention_days
 }
