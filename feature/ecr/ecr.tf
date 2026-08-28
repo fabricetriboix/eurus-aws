3d9c640e47e4bfb93ab3ca3507634a36c4e3f006
Fabrice Triboix <ft+github@falcon1.ltd>
2026-08-28 13:11:14 +0100

    feat(ecr): Cleanup

--- feature/ecr/ecr.tf
+++ feature/ecr/ecr.tf
@@ -115,8 +115,7 @@ data "aws_iam_policy_document" "template_repository_policy" {
 
     principals {
       type        = "AWS"
-      identifiers = ["arn:aws:iam::${local.account_id}:root"]
-      #identifiers = [for id in local.pull_account_ids : "arn:aws:iam::${id}:root"]
+      identifiers = [for id in local.pull_account_ids : "arn:aws:iam::${id}:root"]
     }
 
     actions = [
